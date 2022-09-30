49476cae09893452d1691b869f11b530fce1d32d
sreerag-pavithran <sreeragpavithran@gmail.com>
2022-09-30 19:51:10 +0530

    LINK

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://206.189.206.202/api/v1";
+export const API_URL = "https://golivegameplan.com/api/v1";
 // "https://walrus-app-z3mvj.ondigitalocean.app/ops-backend-new2/api/v1";
