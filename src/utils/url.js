f483f3cbc799b24a729d41e3172c7d2e6bdeb557
sreerag-pavithran <sreeragpavithran@gmail.com>
2022-10-08 21:24:02 +0530

    Email

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,2 @@
-export const API_URL =
-  // "https://golivegameplan.com/api/v1";
-  "http://localhost:5000/api/v1";
+export const API_URL = "https://golivegameplan.com/api/v1";
+// "http://localhost:5000/api/v1";
